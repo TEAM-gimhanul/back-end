198726ee7270e0c9ae61859f835a864ed61b99ad
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-24 17:45:20 +0900

    Fix: member service DI

--- src/main/java/com/backend/gimhanul/domain/member/service/MemberService.java
+++ src/main/java/com/backend/gimhanul/domain/member/service/MemberService.java
@@ -1,4 +1,12 @@
 package com.backend.gimhanul.domain.member.service;
 
+import com.backend.gimhanul.domain.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+  private final MemberRepository memberRepository;
 }
