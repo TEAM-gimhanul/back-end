8197f7e53c8e508c29ad9c235bcf379bb0cc8d99
leeseojune <sung07288346@gmail.com>
2021-11-25 00:42:17 +0900

    📑 :: GoogleInformationResponse dto

---
+++ src/main/java/com/backend/gimhanul/global/utils/api/dto/response/GoogleInformationResponse.java
@@ -0,0 +1,16 @@
+package com.backend.gimhanul.global.utils.api.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GoogleInformationResponse {
+
+	 private String email;
+
+	 private String name;
+
+	 private String picture;
+
+}
