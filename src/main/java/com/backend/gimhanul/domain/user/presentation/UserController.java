be21d69bc36c7b062931b9d3442b605eb202e9ca
leeseojune <sung07288346@gmail.com>
2021-11-24 23:56:26 +0900

    ⚡ :: 네이버 로그인 Api

--- src/main/java/com/backend/gimhanul/domain/user/presentation/UserController.java
+++ src/main/java/com/backend/gimhanul/domain/user/presentation/UserController.java
@@ -1,11 +1,16 @@
 package com.backend.gimhanul.domain.user.presentation;
 
+import com.backend.gimhanul.domain.user.presentation.dto.request.AuthRequest;
+import com.backend.gimhanul.domain.user.presentation.dto.response.TokenResponse;
+import com.backend.gimhanul.domain.user.service.NaverAuthService;
 import com.backend.gimhanul.domain.user.service.QueryGoogleAuthLinkService;
 import com.backend.gimhanul.domain.user.service.QueryKakaoAuthLinkService;
 import com.backend.gimhanul.domain.user.service.QueryNaverAuthLinkService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +22,7 @@ public class UserController {
 	private final QueryKakaoAuthLinkService queryKakaoAuthLinkService;
 	private final QueryGoogleAuthLinkService queryGoogleAuthLinkService;
 	private final QueryNaverAuthLinkService queryNaverAuthLinkService;
+	private final NaverAuthService naverAuthService;
 
 	@GetMapping("/kakao")
 	public String queryKakaoAuthLink() {
@@ -31,6 +37,11 @@ public class UserController {
 	@GetMapping("/naver")
 	public String queryNaverAuthLink() {
 		return queryNaverAuthLinkService.execute();
+	}
+
+	@PostMapping("/naver")
+	public TokenResponse naverAuth(@RequestBody AuthRequest request) {
+		return naverAuthService.execute(request);
 	}
 
 }
