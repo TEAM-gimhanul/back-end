b25c13787abe55b4483b3d696af97b94b6a92d73
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-24 12:07:25 +0900

    Initalize

---
+++ src/main/java/com/backend/gimhanul/GimhanulApplication.java
@@ -0,0 +1,13 @@
+package com.backend.gimhanul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimhanulApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GimhanulApplication.class, args);
+  }
+
+}
