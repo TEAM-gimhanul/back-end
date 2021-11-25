0eed98e068a977e52dc8e2b59dc04396640cb84f
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-26 01:19:19 +0900

    Add: check only special

--- src/main/java/com/backend/gimhanul/global/utils/CheckExist.java
+++ src/main/java/com/backend/gimhanul/global/utils/CheckExist.java
@@ -5,12 +5,18 @@ import java.util.regex.Pattern;
 public class CheckExist {
 
     private final static String ONLY_ENG = "^[a-zA-Z]*$";
+    private final static String ONLY_SPECIAL = "^[`~!@#$%^&*()_+=-]*$";
 
     private static boolean checkOnlyEng (String message) {
         return !Pattern.matches(ONLY_ENG, message);
     }
 
+    private static boolean checkOnlySpecial (String message) {
+        return !Pattern.matches(ONLY_SPECIAL, message);
+    }
+
     public static boolean checkIsOnlyEng (String message) {
-        return checkOnlyEng(message);
+        return checkOnlyEng(message)
+                || checkOnlySpecial(message);
     }
 }
