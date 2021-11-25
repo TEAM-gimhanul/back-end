2e86f6a389fe6ad0c3173c1c97fae034a75bde37
leeseojune <sung07288346@gmail.com>
2021-11-25 09:53:06 +0900

    📑 :: SubscribeAllService

---
+++ src/main/java/com/backend/gimhanul/domain/chat/service/SubscribeAllService.java
@@ -0,0 +1,25 @@
+package com.backend.gimhanul.domain.chat.service;
+
+import com.backend.gimhanul.domain.chat.domain.repository.RoomRepository;
+import com.backend.gimhanul.domain.user.domain.User;
+import com.backend.gimhanul.domain.user.facade.UserFacade;
+import com.corundumstudio.socketio.SocketIOClient;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SubscribeAllService {
+
+	private final RoomRepository roomRepository;
+	private final UserFacade userFacade;
+
+	public void execute(SocketIOClient client) {
+		User user = userFacade.findUserByClient(client);
+
+		roomRepository.findIdEmail(user.getId())
+				.forEach(client::joinRoom);
+	}
+
+}
