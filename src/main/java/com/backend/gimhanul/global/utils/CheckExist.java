5ad7976cb399f4e1db6ffcd6f02c2a7b279109ec
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-26 01:36:18 +0900

    Fix: check Logic

--- src/main/java/com/backend/gimhanul/global/utils/CheckExist.java
+++ src/main/java/com/backend/gimhanul/global/utils/CheckExist.java
@@ -4,25 +4,28 @@ import java.util.regex.Pattern;
 
 public class CheckExist {
 
-    private final static String ONLY_ENG = "^[a-zA-Z]*$";
-    private final static String ONLY_SPECIAL = "^[`~!@#$%^&*()_+=-]*$";
-    private final static String ONLY_NUM = "^[0-9]*$";
+    private final static String CHECK_KR = ".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*";
+//    private final static String ONLY_ENG = "^[a-zA-Z]*$";
+//    private final static String ONLY_SPECIAL = "^[`~!@#$%^&*()_+=-]*$";
+//    private final static String ONLY_NUM = "^[0-9]*$";
 
-    private static boolean checkOnlyEng (String message) {
-        return Pattern.matches(ONLY_ENG, message);
+    private static boolean checkKR(String message) {
+        return Pattern.matches(CHECK_KR, message);
     }
 
-    private static boolean checkOnlySpecial (String message) {
-        return Pattern.matches(ONLY_SPECIAL, message);
-    }
+//    private static boolean checkOnlyEng (String message) {
+//        return Pattern.matches(ONLY_ENG, message);
+//    }
+//
+//    private static boolean checkOnlySpecial (String message) {
+//        return Pattern.matches(ONLY_SPECIAL, message);
+//    }
+//
+//    private static boolean checkOnlyNum (String message) {
+//        return Pattern.matches(ONLY_NUM, message);
+//    }
 
-    private static boolean checkOnlyNum (String message) {
-        return Pattern.matches(ONLY_NUM, message);
-    }
-
-    public static boolean checkIsOnlyEng (String message) {
-        return checkOnlyEng(message)
-                || checkOnlySpecial(message)
-                || checkOnlyNum(message);
+    public static boolean checkIsOnlyEng(String message) {
+        return !checkKR(message);
     }
 }
