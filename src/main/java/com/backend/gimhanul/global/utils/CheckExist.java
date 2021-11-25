c154318131fe03612b533e4b786e167d071657ca
krung2 <jungbin4337@dgsw.hs.kr>
2021-11-26 01:21:23 +0900

    Add: check only num

--- src/main/java/com/backend/gimhanul/global/utils/CheckExist.java
+++ src/main/java/com/backend/gimhanul/global/utils/CheckExist.java
@@ -6,17 +6,23 @@ public class CheckExist {
 
     private final static String ONLY_ENG = "^[a-zA-Z]*$";
     private final static String ONLY_SPECIAL = "^[`~!@#$%^&*()_+=-]*$";
+    private final static String ONLY_NUM = "^[0-9]*$";
 
     private static boolean checkOnlyEng (String message) {
-        return !Pattern.matches(ONLY_ENG, message);
+        return Pattern.matches(ONLY_ENG, message);
     }
 
     private static boolean checkOnlySpecial (String message) {
-        return !Pattern.matches(ONLY_SPECIAL, message);
+        return Pattern.matches(ONLY_SPECIAL, message);
+    }
+
+    private static boolean checkOnlyNum (String message) {
+        return Pattern.matches(ONLY_NUM, message);
     }
 
     public static boolean checkIsOnlyEng (String message) {
         return checkOnlyEng(message)
-                || checkOnlySpecial(message);
+                || checkOnlySpecial(message)
+                || checkOnlyNum(message);
     }
 }
