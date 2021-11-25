2ec95c263a43f771ebc469ec200dab6f056d8109
leeseojune <sung07288346@gmail.com>
2021-11-25 22:32:37 +0900

    🐛 :: 특수문자 치환

--- src/main/java/com/backend/gimhanul/global/utils/api/dto/request/FilterRequest.java
+++ src/main/java/com/backend/gimhanul/global/utils/api/dto/request/FilterRequest.java
@@ -1,14 +1,26 @@
 package com.backend.gimhanul.global.utils.api.dto.request;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class FilterRequest {
 
 	private final List<String> text;
+
+	public FilterRequest(List<String> text) {
+		List<String> bufList = new ArrayList<>();
+
+		for(String value : text) {
+			if(value.getBytes().length <= 1)
+				bufList.add(value.replace(value, "쩝"));
+			else
+				bufList.add(value);
+		}
+
+		this.text = bufList;
+	}
 
 }
