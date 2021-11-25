262c7140b12c08bd1aa05acced4015ab34244b8d
leeseojune <sung07288346@gmail.com>
2021-11-26 00:58:31 +0900

    📑 :: Swear

---
+++ src/main/java/com/backend/gimhanul/domain/chat/domain/Swear.java
@@ -0,0 +1,32 @@
+package com.backend.gimhanul.domain.chat.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "tbl_swear")
+public class Swear {
+
+	@Id
+	private String id;
+
+	private int count;
+
+	private boolean isSwear;
+
+	private String replaceMessage;
+
+	@Builder
+	public Swear(String id, int count, boolean isSwear, String replaceMessage) {
+		this.id = id;
+		this.count = count;
+		this.isSwear = isSwear;
+		this.replaceMessage = replaceMessage;
+	}
+
+}
