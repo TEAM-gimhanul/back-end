49bd4e7d15b7e5f43d34c0c50b781407274e9633
leeseojune <sung07288346@gmail.com>
2021-11-25 09:41:07 +0900

    ⚡ :: findByUserAndRoom

--- src/main/java/com/backend/gimhanul/domain/chat/domain/repository/MemberRepository.java
+++ src/main/java/com/backend/gimhanul/domain/chat/domain/repository/MemberRepository.java
@@ -1,9 +1,15 @@
 package com.backend.gimhanul.domain.chat.domain.repository;
 
+import java.util.Optional;
+
 import com.backend.gimhanul.domain.chat.domain.Member;
+import com.backend.gimhanul.domain.chat.domain.Room;
+import com.backend.gimhanul.domain.user.domain.User;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
+	Optional<Member> findByUserAndRoom(User user, Room room);
 }
